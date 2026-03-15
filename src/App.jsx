ed023bcd8f0abfb3c8f41440645753bdc2d1bac0
Palak Jagpal <palakjagpal70@gmail.com>
2026-03-15 14:29:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Text_summary from './Text_summary';
+
+function App() {
+  return (
+    <>
+      <Text_summary/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Text_summary.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import axios from "axios"; 
+import "./Text_summary.css"; 
+
+function Text_summary(){
+    const [text, settext] =  useState("");
+    const [summary, setsummary] = useState(null);
+    const [error, seterror] = useState("");
+    const [limit, setlimit] =  useState(0);
+    const limit_count = 5;
+
+    const api_key = import.meta.env.VITE_API_KEY;
+
+    //OpenAI
+    //const api_url = "https://api.openai.com/v1/chat/completions";
+
+    //OpenRouter
+    //const api_url="https://openrouter.ai/api/v1/chat/completions";
+
+    //Apyhub API
+    const api_url = "https://api.apyhub.com/ai/summarize-text";
+
+    async function fetchSummary(e){
+        e.preventDefault();
+
+        if(limit >= limit_count){
+            seterror("Daily summary limit reached. Please try again tomorrow.");
+            console.log("Daily summary limit reached. Please try again tomorrow.");
+            return;
+        }
+
+        if(text.trim() === ""){
+            console.log("Enter or paste a text to summarize");
+            seterror("Enter or paste a text to summarize");
+            return;
+        }
+
+        try{
+            const response = await axios.post(api_url,
+                { text : text },
+                {
+                    headers : {
+                        "apy-token" : api_key,
+                        "Content-Type" : "application/json" 
+                    }
+                }
+            );
+            const result = response.data.data.summary;
+            setsummary(result);
+            setlimit(limit_count+1);
+            console.log("Summary generated successfully");
+            console.log("Limit : ",limit);
+        }catch(err){
+            console.log("Error : " , err);
+            seterror("Failed to generate summary");
+        }
+    }
+
+    return(
+        <>
+
+            {error && (
+                <div className="error">
+                    <p className="err">{error}</p>
+                </div>
+            )}
+
+            <div className="main">
+                <div className="title">
+                    <h1>SnapSummary</h1>
+                    <p>“Your shortcut to smarter reading”</p>
+                </div>
+                <div>
+                    <form onSubmit={fetchSummary}>
+                        <textarea placeholder="Paste or enter the text you want to summarize....." 
+                        onChange={(e) => {
+                            settext(e.target.value);
+                            seterror("");
+                        }}></textarea>
+                        <p className="limit">{limit}/{limit_count}</p>
+                        <button type="submit">GENERATE SUMMARY</button>
+                    </form>
+                </div>
+
+                {!error && summary && (
+                    <div className="summary">
+                        <h3>Summary : </h3>
+                        <p className="summ">{summary}</p>
+                    </div>
+                )}
+            </div>
+        </>
+    )
+}
+
+export default Text_summary;
